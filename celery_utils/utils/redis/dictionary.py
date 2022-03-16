a5781faa1c54b1b17eab37017b6170ab1b1f3e7e
Jenya Sovetkin <e.sovetkin@gmail.com>
2022-03-16 11:12:20 +0100

    utils: redis dictionary and lock

---
+++ celery_utils/utils/redis/dictionary.py
@@ -0,0 +1,58 @@
+import json
+import redis
+
+from celery_utils.utils.float_hash \
+    import float_hash
+
+from celery_utils.utils.redis.parse_url \
+    import parse_url
+
+
+class Redis_Dictionary:
+
+
+    def __init__(self, name, redis_url,
+                 hash_function = float_hash,
+                 expire_time = 7200):
+        """A distributed dictionary with redis
+
+        :name: name of the set in redis
+
+        :redis_url: how to connect to redis
+
+        :hash_function: function that produces a hash for keys
+
+        :expire_time: time to expire for dictionary items
+        """
+        self._name = name
+        self._client = redis.StrictRedis(**parse_url(redis_url))
+        self._hash = hash_function
+        self._expire = expire_time
+
+
+    def __contains__(self, key):
+        hkey = self._hash(key)
+        return self._client.exists(self._name + hkey)
+
+
+    def __setitem__(self, key, item):
+        hkey = self._hash(key)
+        sitem = json.dumps(item)
+        self._client.set(self._name + hkey, sitem,
+                         ex = self._expire)
+
+
+    def __getitem__(self, key):
+        hkey = self._hash(key)
+
+        if not self._client.exists(self._name + hkey):
+            raise KeyError\
+                ("'{key}' is not in hset".format(key=key))
+
+        sitem = self._client.get(self._name + hkey)
+        return json.loads(sitem)
+
+
+    def __delitem__(self, key):
+        hkey = self._hash(key)
+        self._client.delete(self._name + hkey)
