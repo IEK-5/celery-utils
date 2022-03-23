77bd832a6e258ffbcf8090187966aac1096b3102
Jenya Sovetkin <e.sovetkin@gmail.com>
2022-03-23 11:15:22 +0100

    README, LICENSE, packaging and requirements.txt

---
+++ README.md
@@ -0,0 +1,7 @@
+# Tools to make distributed services with celery
+
+The `celery_utils` provides a way to create distributed services. We use the [celery](https://github.com/celery/celery) queue management system, hence the name of our tool.
+
+`celery_utils` package turns any python code into a distributed web service. It allows organising functions into a tree of tasks, where task results are cached in shared storage.
+
+[Wiki](https://github.com/esovetkin/celery-utils/wiki) pages contain detailed documentation and examples.
---
+++ setup.py
@@ -0,0 +1,43 @@
+import setuptools
+import os
+import stat
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+with open('requirements.txt','r') as f:
+    requirements = [x.strip() for x in f.readlines()]
+
+
+def find_executable(path):
+    executable = stat.S_IEXEC | stat.S_IXGRP | \
+                 stat.S_IXOTH | (not stat.S_ISLNK)
+
+    res = []
+    for dp, dn, filenames in os.walk(path):
+        if '.git/' in dp:
+            continue
+
+        for f in filenames:
+            if not (os.stat(os.path.join(dp,f)).st_mode & executable):
+                continue
+
+            res += [os.path.join(dp,f)]
+
+    return res
+
+
+setuptools.setup(
+    name='celery-utils',
+    version='0.1',
+    author='Evgenii Sovetkin',
+    author_email='e.sovetkin@gmail.com',
+    description='Tools to work with celery tasks and store results in remote storage',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='git@github.com:esovetkin/celery-utils',
+    packages=setuptools.find_packages(),
+    scripts=find_executable('scripts/'),
+    install_requires=requirements)
