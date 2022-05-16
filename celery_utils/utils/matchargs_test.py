b2e6ba5cbd08a094a09514ffd9fabf6e7f88a4a4
Jenya Sovetkin <e.sovetkin@gmail.com>
2022-05-16 11:08:44 +0200

    minor: set license headers

--- celery_utils/utils/matchargs_test.py
+++ celery_utils/utils/matchargs_test.py
@@ -1,3 +1,21 @@
+#
+# This file is part of the celery-utils (https://github.com/e.sovetkin/celery-utils).
+# Copyright (c) 2022 Jenya Sovetkin.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+
 import pytest
 
 from celery_utils.utils.matchargs \
